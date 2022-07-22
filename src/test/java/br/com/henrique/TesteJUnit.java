985779a847279ba8d7a42916d299cf71aadcbd76
Carlos Henrique <carlos.henrique.mobile@gmail.com>
2022-07-21 23:50:58 -0300

    JUnint - Iniciando implementação de Testes Automatizados

---
+++ src/test/java/br/com/henrique/TesteJUnit.java
@@ -0,0 +1,13 @@
+package br.com.henrique;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TesteJUnit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
